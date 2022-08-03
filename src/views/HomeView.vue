6ea9e34388c1d74c3896feae5df108a0d6d1c90b
Gustavo Barrera <gbarrerameneses@gmail.com>
2022-08-03 16:01:38 -0500

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <form @submit.prevent="procesarFormulario">
+  <!-- v-model="text" -->
+  <input 
+  type="text"
+  class="form-control my-2"
+  placeholder="Ingrese Nombre"
+  v-model.trim="tarea.nombre">
+<!-- v-model="checkbox" -->
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="tarea.categorias"
+    value="javascript">
+    <label for="check-1" class="form-check-label"> JavaScript</label>
   </div>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-2"
+    class="form-check-input"
+    v-model="tarea.categorias"
+    value="nodejs">
+    <label for="check-2" class="form-check-label"> Node.js</label>
+  </div>
+<!-- v-model="radio" -->
+  <div class="mt-2">
+    <div class="form-check form-check-inline">
+      <input 
+      type="radio"
+      id="radio-1"
+      class="form-check-input"
+      v-model="tarea.estado"
+      value="urgente">
+      <label for="radio-1" class="form-check-label"> Urgente</label>
+    </div>  
+    <div class="form-check form-check-inline">
+      <input 
+      type="radio"
+      id="radio-2"
+      class="form-check-input"
+      v-model="tarea.estado"
+      value="relax">
+      <label for="radio-2" class="form-check-label"> Relax</label>
+    </div> 
+  </div>
+<!-- v-model="number" -->
+  <div class="mt-2">
+    <input type="number"
+    class="form-control"
+    v-model.number="tarea.numero">
+  </div>
+  <button 
+    class="btn btn-dark mt-2 btn-block" 
+    type="submit"
+    :disabled="bloquear">Procesar</button>
+ </form>
+ <hr>
+ <p>{{ tarea }}</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  methods: {
+  procesarFormulario(){
+    if(this.tarea.nombre.trim() === ''){
+      console.log('nombre Vacío')
+      return
+    }
+      console.log('no está vacío')
+      //se envían los datos
+
+      this.tarea = {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  computed:{
+      bloquear(){
+        return this.tarea.nombre.trim() === '' ? true : false
+      }
+  },
 }
 </script>
